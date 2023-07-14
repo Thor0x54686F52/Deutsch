e9dff97e832753d49c0a8da5333f6a7221d69cba
Raphael Oberhummer <raphael.oberhummer@gmail.com>
2023-07-14 22:04:34 +0200

    Grundlage für Kontrollmechanismus gelegt, Grundlagen für Verzweigungen

---
+++ Kompilieren/Kontrolle/kontrolle.cpp
@@ -0,0 +1,19 @@
+#include "kontrolle.hpp"
+
+std::string Text_kontrolieren(std::string Text) { // Kontroliert Gramatik, Rechtschreibung und Stil
+   std::string text = "";
+   // Logik muss noch implemetiert werden
+   return text;
+}
+
+std::string Code_kontrolieren(std::string Code){ // Kontrolliert C++ Code ob der Kompilierbar ist
+   std::string code;
+   // Logik muss noch implemetiert werden
+   return code;
+}
+
+std::string Formatieren(std::string Text) { // Formatiert den generierten C++ Code in "schönen" C++ Code
+   std::string text;
+   // Logik muss noch implemetiert werden
+   return text;
+}
